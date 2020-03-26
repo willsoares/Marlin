5cd0a213a16174e767c498a366b3656e632f6359
willsoares <willsoares96@hotmaiil.com>
2020-03-26 18:42:35 -0300

    Changing the preheat temp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1479,7 +1479,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 180
+#define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
